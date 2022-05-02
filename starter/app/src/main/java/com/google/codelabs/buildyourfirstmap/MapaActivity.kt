6d72f276a835ee922d7cc8f7b15634f65a7cb970
Daniel <dangalcar@alum.us.es>
2022-05-02 20:18:42 +0200

    mapa implement

--- starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MapaActivity.kt
+++ starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MapaActivity.kt
@@ -1,9 +1,18 @@
 package com.google.codelabs.buildyourfirstmap
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.google.codelabs.buildyourfirstmap.databinding.ActivityMapaBinding
@@ -11,7 +20,13 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.FirebaseFirestore
 
-class MapaActivity: AppCompatActivity() {
+class MapaActivity: AppCompatActivity(), OnMapReadyCallback {
+
+    // Nuestro mapa
+    private lateinit var map: GoogleMap
+
+    // Variable para ver si los permisos han sido denegados
+    private var permissionDenied = false
 
     private lateinit var binding: ActivityMapaBinding
     private lateinit var firebaseAuth: FirebaseAuth
@@ -22,6 +37,8 @@ class MapaActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMapaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        generateFragment()
 
         firebaseAuth = FirebaseAuth.getInstance()
         val firebaseUser = firebaseAuth.currentUser
@@ -59,6 +76,86 @@ class MapaActivity: AppCompatActivity() {
 
     }
 
+    // Función que crea el fragmento del mapa
+    private fun generateFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    // Esto  se llama cuando el mapa haya sido creado
+    override fun onMapReady(gMap: GoogleMap) {
+        map = gMap
+        enableMyLocation()
+
+    }
+
+    /**
+     * Permite el layer de tu localización si los permisos de localización
+     * han sido garantizados
+     */
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if(!::map.isInitialized) return
+        // Si se han aceptado los permisos
+        if(isLocationPermissionGranted()){
+            map.isMyLocationEnabled = true
+
+        } else{ // Si no
+            requestLocationPermission()
+        }
+
+    }
+
+    private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
+        this,Manifest.permission.ACCESS_FINE_LOCATION
+    ) ==  PackageManager.PERMISSION_GRANTED
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this,"Ve a ajustes y acepta los permisos",Toast.LENGTH_SHORT).show()
+        } else{
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE)
+        }
+
+
+    }
+
+    // [START maps_check_location_permission_result]
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode != LOCATION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+        if (isLocationPermissionGranted()) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation()
+        } else {
+            permissionDenied = true
+
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError()
+            permissionDenied = false
+        }
+    }
+
+    private fun showMissingPermissionError() {
+        //newInstance(true).show(supportFragmentManager, "dialog")
+        Toast.makeText(this,"Ve a ajustes y acepta los permisos",Toast.LENGTH_SHORT).show()
+    }
+
+
+    // Variable quen nos dice el código de localización
+    companion object {
+        const val LOCATION_REQUEST_CODE = 1
+    }
 
     private fun checkUser(): String {
         //check user is logged in or not
