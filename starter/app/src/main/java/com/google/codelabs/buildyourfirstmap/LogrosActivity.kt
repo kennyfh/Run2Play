587ac93edc87a49f8c44497c9632150d975b44a9
Edugonzaper <eddugonzzaperrez@gmail.com>
2022-05-03 10:36:31 +0200

    pre dinamical querys

--- starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/LogrosActivity.kt
+++ starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/LogrosActivity.kt
@@ -1,8 +1,11 @@
 package com.google.codelabs.buildyourfirstmap
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,14 +14,22 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.google.codelabs.buildyourfirstmap.databinding.ActivityLogrosBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Comment
 
 class LogrosActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogrosBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var database : DatabaseReference
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +40,7 @@ class LogrosActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         val firebaseUser = firebaseAuth.currentUser
         val uId = checkUser()
+        database = Firebase.database.reference
         checkName(uId)
 
         initRecyclerView()
@@ -65,10 +77,21 @@ class LogrosActivity : AppCompatActivity() {
     private fun initRecyclerView(){
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerLogros)
         recyclerView.layoutManager = GridLayoutManager(this, 4)
-        recyclerView.adapter = LogrosAdapter(logrosProvider.setup())
+        var kyara = LogrosAdapter(logrosProvider.logritos)
+        val handler = Handler()
+        handler.postDelayed({
+            // your code to run after 2 second
+            recyclerView.adapter = kyara
+        }, 2000)
+
+
     }
 
-    private fun checkUser(): String {
+
+
+
+
+        private fun checkUser(): String {
         //check user is logged in or not
         val firebaseUser = firebaseAuth.currentUser
         if(firebaseUser != null){
--- starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/logrosProvider.kt
+++ starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/logrosProvider.kt
@@ -5,86 +5,80 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.FirebaseFirestore
 
-class logrosProvider {
-    companion object {
-
-        private lateinit var firebaseAuth: FirebaseAuth
-        private lateinit var database : DatabaseReference
-
-        @JvmStatic fun setup(): List<Logro> {
-            var logrosList = listOf<Logro>()
+object logrosProvider {
 
 
-            fun checkUser(): String {
-                //check user is logged in or not
-                val firebaseUser = firebaseAuth.currentUser
-                if(firebaseUser != null){
-
-                    //user not null,user is logged in, get user info
-                    val uid = firebaseUser.uid
-                    //set to text view
-                    return uid.toString()
-
-
-                }
-                else {
-
-
-                    return ""
-                }
-
-            }
-
-
-            val userName = checkUser()
-            //llamamos a la base de datos
-            val db = FirebaseFirestore.getInstance()
-            //sacamos la colección usuarios para obtener los elementos buscados
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var database : DatabaseReference
+    var logrosList = listOf<Logro>()
+    var logritos = checkUser()
 
 
 
-            var currency = ""
-
-            db.collection("userAchievements").whereEqualTo("user",userName).get()
-                .addOnSuccessListener { documents ->
-                    for (document in documents){
-
-                        db.collection("achievements").document(document.data["achievement"].toString()).get()
-                            .addOnSuccessListener { newDocuments ->
-
-                                var title = newDocuments.data?.get("title")?.toString()
-                                var url = newDocuments.data?.get("iconActive")?.toString()
-                                var description = newDocuments.data?.get("description")?.toString()
+    fun checkUser(): List<Logro> {
+        //se le puede pasar como parametro
+        /*
+        val firebaseUser = firebaseAuth.currentUser
 
 
 
 
-                                logrosList.plus(Logro(title.toString(),description.toString()))
-                                Log.w("tag", "${document.id} => ${newDocuments.data?.get("title")?.toString()} ")
-                            }
+        if(firebaseUser != null){
 
-                            .addOnFailureListener { exception->
-                                Log.w("tag", "Error getting documents " , exception)
-                            }
-
-
-
-
-
-
-
-                    }
-                }
-                .addOnFailureListener { exception->
-                    Log.w("tag", "Error getting documents " , exception)
-
-                }
-
-
-            return logrosList
-
+            //user not null,user is logged in, get user info
+            val uid = firebaseUser.uid
+            //set to text view
+            userName = uid.toString()
 
 
         }
+        */
+
+        //llamamos a la base de datos
+        val db = FirebaseFirestore.getInstance()
+        //sacamos la colección usuarios para obtener los elementos buscados
+
+
+
+        var userName = "IGuowIU1wuPt6SjUiKDfXpFXk6K2"
+
+        db.collection("userAchievements").whereEqualTo("user",userName).get()
+        .addOnSuccessListener { documents ->
+            for (document in documents){
+
+                db.collection("achievements").document(document.data["achievement"].toString()).get()
+                    .addOnSuccessListener { newDocuments ->
+
+                        var title = newDocuments.data?.get("title")?.toString()
+                        var url = newDocuments.data?.get("iconActive")?.toString()
+                        var description = newDocuments.data?.get("description")?.toString()
+
+
+
+
+                        logrosList += Logro(title.toString(),description.toString())
+                        Log.w("tag", "${document.id} => ${logrosList.get(0)} ")
+                        Log.w("tag", "${document.id} => ${newDocuments.data?.get("title")?.toString()} ")
+                    }
+
+                    .addOnFailureListener { exception->
+                        Log.w("tag", "Error getting documents " , exception)
+                    }
+
+
+
+
+
+
+
+            }
+        }
+        .addOnFailureListener { exception->
+            Log.w("tag", "Error getting documents " , exception)
+
+        }
+
+        return logrosList
+
     }
 }
