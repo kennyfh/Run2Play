ef76ca4d44362b84fa36b3f2f505f3a4d67c927b
Daniel <dangalcar@alum.us.es>
2022-05-12 23:27:30 +0200

    reto running

--- starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/RetoRunningActivity.kt
+++ starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/RetoRunningActivity.kt
@@ -1,4 +1,11 @@
 package com.google.codelabs.buildyourfirstmap
 
-class RetoRunningActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RetoRunningActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reto_running)
+    }
 }
