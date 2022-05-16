3d2391bf3e2814021016caebbd1d1e915b089d9c
Kenny F <kennyjesus@protonmail.com>
2022-05-16 13:54:41 +0200

    Add Inventory CardView

--- starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/InventoryActivity.kt
+++ starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/InventoryActivity.kt
@@ -2,10 +2,13 @@ package com.google.codelabs.buildyourfirstmap
 
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.google.codelabs.buildyourfirstmap.databinding.ActivityInventoryBinding
@@ -14,6 +17,7 @@ import com.google.firebase.database.*
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import java.io.File
+import android.widget.LinearLayout.LayoutParams
 
 class InventoryActivity : AppCompatActivity() {
 
@@ -27,14 +31,22 @@ class InventoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityInventoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        generateCard()
+        generateCard()
+        generateCard()
+        generateCard()
+        generateCard()
+        generateCard()
+
+
 
         firebaseAuth = FirebaseAuth.getInstance()
         val firebaseUser = firebaseAuth.currentUser
-        val uId = checkUser()
+//        val uId = checkUser()
 
 //        checkName(uId)
 
-        getUserData()
+//        getUserData()
 
 
 
@@ -66,85 +78,142 @@ class InventoryActivity : AppCompatActivity() {
 //            false
 //        })
     }
+// TODO Arreglar tarjeta para que se vea bonita
+    private fun generateCard() {
+        val card_view = CardView(this)
+        val layoutParams = LayoutParams(
+            LayoutParams.MATCH_PARENT, // CardView width
+            LayoutParams.WRAP_CONTENT // CardView height
+        )
+
+        // Set bottom margin for card view
+        layoutParams.bottomMargin = 50
+
+        // Set the card view layout params
+        card_view.layoutParams = layoutParams
+
+        // Set the card view corner radius
+        card_view.radius = 12F
+
+        // Set the card view content padding
+        card_view.setContentPadding(25,25,25,25)
+
+        // Set the card view background color
+        card_view.setCardBackgroundColor(Color.LTGRAY)
+
+        // Set card view elevation
+        card_view.cardElevation = 8F
+
+        // Set card view maximum elevation
+        card_view.maxCardElevation = 12F
+
+        // AQUI IRÁ EL POPUP
+        card_view.setOnClickListener{
+            Toast.makeText(
+                applicationContext,
+                "Card clicked.",
+                Toast.LENGTH_SHORT).show()
+        }
+
+        val quote = TextView(this)
+        quote.text = "\"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.\n";
+        quote.textSize = 24f
+        quote.setTextColor(Color.WHITE)
+        quote.setTypeface(Typeface.SANS_SERIF,Typeface.NORMAL)
+        card_view.addView(quote)
+        // Add an ImageView to the CardView
+//        card_view.addView(generateImageView())
+        binding.layoutInventory.addView(card_view)
+
+    }
+
+    private fun generateImageView() :ImageView{
+        val imageView = ImageView(this)
+        val params = LayoutParams(LayoutParams.WRAP_CONTENT,350)
+        imageView.layoutParams = params
+        imageView.setImageResource(R.drawable.ic_baseline_map_24)
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+        return imageView
+    }
 
 
-    private fun getUserData(){
-        var cnt = 0
-        var cntImage = 0
-        var userName = "IGuowIU1wuPt6SjUiKDfXpFXk6K2"
-        val logName = "X48lZc4Uh3gLT7FWnhFl"
-        val db = FirebaseFirestore.getInstance()
-        db.collection("achievements").orderBy("achievementNumber").get()
-            .addOnSuccessListener { documents ->
-                for (document in documents){
-                    Log.w("achiev", "${document["achievementNumber"]} => omg")
-
-                    val lLay = binding.achievementsTable.getChildAt(cnt) as LinearLayout
-                    val tViwy = lLay.getChildAt(1) as TextView
-                    //val tImg = lLay.getChildAt(0) as ImageView
-                    tViwy.text = document.data?.get("title")?.toString()
-
-                    db.collection("userAchievements").whereEqualTo("mix",userName+document.id).get().addOnSuccessListener {
-                        users ->
-
-                        if (users.isEmpty){
-                            val iLay = binding.achievementsTable.getChildAt(cntImage) as LinearLayout
-                            val tImg = iLay.getChildAt(0) as ImageView
-                            Log.w("achiev", "gonna set inactive ${cntImage+1} => omg")
-                            val imageName = "iconInactive" + (cntImage + 1) + ".png"
-                            val storageRef =
-                                FirebaseStorage.getInstance().reference.child("Images/$imageName")
-
-                            val localfile = File.createTempFile("tempImage", "png")
-                            storageRef.getFile(localfile).addOnSuccessListener {
-
-                                val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
-                                tImg.setImageBitmap(bitmap)
-
-                            }
-
-                            cntImage += 1
-
-                        }
-
-                        else{
-                            Log.w("achiev", "gonna set active ${cntImage+1} => omg")
-                            val iLay = binding.achievementsTable.getChildAt(cntImage) as LinearLayout
-                            val tImg = iLay.getChildAt(0) as ImageView
-                            val imageName = "iconActive" + (cntImage + 1) + ".png"
-                            val storageRef =
-                                FirebaseStorage.getInstance().reference.child("Images/$imageName")
-
-                            val localfile = File.createTempFile("tempImage", "png")
-                            storageRef.getFile(localfile).addOnSuccessListener {
-
-                                val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
-                                tImg.setImageBitmap(bitmap)
-
-                            }
-
-                            cntImage += 1
-                        }
-
-
-
-                    }
-
-
-
-
-                    cnt += 1
-
-
-
-
-
-                }
-            }
-            .addOnFailureListener { exception->
-                Log.w("tag", "Error getting documents " , exception)
-
-            }
+//    private fun getUserData(){
+//        var cnt = 0
+//        var cntImage = 0
+//        var userName = "IGuowIU1wuPt6SjUiKDfXpFXk6K2"
+//        val logName = "X48lZc4Uh3gLT7FWnhFl"
+//        val db = FirebaseFirestore.getInstance()
+//        db.collection("achievements").orderBy("achievementNumber").get()
+//            .addOnSuccessListener { documents ->
+//                for (document in documents){
+//                    Log.w("achiev", "${document["achievementNumber"]} => omg")
+//
+//                    val lLay = binding.achievementsTable.getChildAt(cnt) as LinearLayout
+//                    val tViwy = lLay.getChildAt(1) as TextView
+//                    //val tImg = lLay.getChildAt(0) as ImageView
+//                    tViwy.text = document.data?.get("title")?.toString()
+//
+//                    db.collection("userAchievements").whereEqualTo("mix",userName+document.id).get().addOnSuccessListener {
+//                        users ->
+//
+//                        if (users.isEmpty){
+//                            val iLay = binding.achievementsTable.getChildAt(cntImage) as LinearLayout
+//                            val tImg = iLay.getChildAt(0) as ImageView
+//                            Log.w("achiev", "gonna set inactive ${cntImage+1} => omg")
+//                            val imageName = "iconInactive" + (cntImage + 1) + ".png"
+//                            val storageRef =
+//                                FirebaseStorage.getInstance().reference.child("Images/$imageName")
+//
+//                            val localfile = File.createTempFile("tempImage", "png")
+//                            storageRef.getFile(localfile).addOnSuccessListener {
+//
+//                                val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+//                                tImg.setImageBitmap(bitmap)
+//
+//                            }
+//
+//                            cntImage += 1
+//
+//                        }
+//
+//                        else{
+//                            Log.w("achiev", "gonna set active ${cntImage+1} => omg")
+//                            val iLay = binding.achievementsTable.getChildAt(cntImage) as LinearLayout
+//                            val tImg = iLay.getChildAt(0) as ImageView
+//                            val imageName = "iconActive" + (cntImage + 1) + ".png"
+//                            val storageRef =
+//                                FirebaseStorage.getInstance().reference.child("Images/$imageName")
+//
+//                            val localfile = File.createTempFile("tempImage", "png")
+//                            storageRef.getFile(localfile).addOnSuccessListener {
+//
+//                                val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+//                                tImg.setImageBitmap(bitmap)
+//
+//                            }
+//
+//                            cntImage += 1
+//                        }
+//
+//
+//
+//                    }
+//
+//
+//
+//
+//                    cnt += 1
+//
+//
+//
+//
+//
+//                }
+//            }
+//            .addOnFailureListener { exception->
+//                Log.w("tag", "Error getting documents " , exception)
+//
+//            }
 
        // firebaseAuth = FirebaseAuth.getInstance()
         //val firebaseUser = firebaseAuth.currentUser
@@ -172,7 +241,7 @@ class InventoryActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         })*/
-    }
+//    }
 
 
 
@@ -194,25 +263,25 @@ class InventoryActivity : AppCompatActivity() {
 
 
 
-        private fun checkUser(): String {
-        //check user is logged in or not
-        val firebaseUser = firebaseAuth.currentUser
-        if(firebaseUser != null){
-
-            //user not null,user is logged in, get user info
-            val uid = firebaseUser.uid
-            //set to text view
-            return uid.toString()
-
-
-        }
-        else {
-
-
-            return ""
-        }
-
-    }
+//        private fun checkUser(): String {
+//        //check user is logged in or not
+//        val firebaseUser = firebaseAuth.currentUser
+//        if(firebaseUser != null){
+//
+//            //user not null,user is logged in, get user info
+//            val uid = firebaseUser.uid
+//            //set to text view
+//            return uid.toString()
+//
+//
+//        }
+//        else {
+//
+//
+//            return ""
+//        }
+//
+//    }
 
 //    private fun checkName(userName: String){
 //
